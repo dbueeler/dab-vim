c1204fc357b3596ecfed716143842265567ee9dc
dab-work <bueelerd@ethz.ch>
2025-12-04 14:22:49 +0100

    Batman.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = 'master',
+	lazy = false, 
+	build = ":TSUpdate",
+
+	opts = {
+		indent = { enable = true },
+		highlight = { enable = true },
+		ensure_installed = {
+			"bash",
+			"c",
+			"javascript",
+			"typescript",
+			"lua",
+			"vim",
+			"toml",
+			"python",
+			"markdown"
+		},
+	},
+}
